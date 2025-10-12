3667ae3d1d4189e6a0e15d615fe5123d96f986d0
CelanMatjaz <matjazcelangithub@gmail.com>
2025-10-12 21:25:36 +0200

    fix nil require value

--- lua/matjaz/plugins/configs/gitsigns.lua
+++ lua/matjaz/plugins/configs/gitsigns.lua
@@ -1,29 +1,28 @@
-local function goto_next_hunk()
-	gitsigns.reset_hunk("next")
-end
-
-local function goto_prev_hunk()
-	gitsigns.reset_hunk("prev")
-end
-
 return {
 	"lewis6991/gitsigns.nvim",
-	config = function ()
+	config = function()
 		local gitsigns = require "gitsigns"
+
+		local function goto_next_hunk()
+			gitsigns.reset_hunk("next")
+		end
+
+		local function goto_prev_hunk()
+			gitsigns.reset_hunk("prev")
+		end
+
 		gitsigns.setup {}
 
 		require "matjaz.mappings.set".set_mappings({
-			{ "<leader>gd",gitsigns.diffthis,                    mode = "n", desc = "Diff this buffer" },
-			{ "<leader>gw",gitsigns.toggle_word_diff,            mode = "n", desc = "Toggle word diff" },
-			{ "<leader>gbl",gitsigns.toggle_current_line_blame,   mode = "n", desc = "Toggle line blame" },
-			{ "<leader>ghs",gitsigns.stage_hunk,                 mode = "n", desc = "Stage hunk" },
-			{ "<leader>ghr",gitsigns.reset_hunk,                 mode = "n", desc = "Reset hunk" },
-			{ "<leader>ghn",goto_next_hunk,      	           mode = "n", desc = "Go to next hunk" },
-			{ "<leader>ghN",goto_prev_hunk,                 mode = "n", desc = "Go to prev hunk" },
-			{ "<leader>gbs",gitsigns.stage_buffer,               mode = "n", desc = "Stage buffer" },
-			{ "<leader>gbr",gitsigns.reset_buffer,               mode = "n", desc = "Reset buffer" },
+			{ "<leader>gd",  gitsigns.diffthis,                  mode = "n", desc = "Diff this buffer" },
+			{ "<leader>gw",  gitsigns.toggle_word_diff,          mode = "n", desc = "Toggle word diff" },
+			{ "<leader>gbl", gitsigns.toggle_current_line_blame, mode = "n", desc = "Toggle line blame" },
+			{ "<leader>ghs", gitsigns.stage_hunk,                mode = "n", desc = "Stage hunk" },
+			{ "<leader>ghr", gitsigns.reset_hunk,                mode = "n", desc = "Reset hunk" },
+			{ "<leader>ghn", goto_next_hunk,                     mode = "n", desc = "Go to next hunk" },
+			{ "<leader>ghN", goto_prev_hunk,                     mode = "n", desc = "Go to prev hunk" },
+			{ "<leader>gbs", gitsigns.stage_buffer,              mode = "n", desc = "Stage buffer" },
+			{ "<leader>gbr", gitsigns.reset_buffer,              mode = "n", desc = "Reset buffer" },
 		})
 	end
 }
-
-
